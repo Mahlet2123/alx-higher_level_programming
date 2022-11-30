33be7df6fa5a3255c91284f9b6e0f608931e9711
mahlet <akotet2123@gmail.com>
2022-11-30 15:25:00 -0800

    task 0x01

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 for num1 in range(0, 10):
     for num2 in range(0, 10):
         if num1 < num2:
-            print('{}{}, '.format(num1, num2), end='')
+            print('{}{}'.format(num1, num2), end='')
+            if (not(num1 == 8 and num2 == 9)):
+                print(", ", end='')
 print("")
