0ead6c57103feecf0d3a75aa5763ed957899f817
mahlet <akotet2123@gmail.com>
2022-11-30 16:57:25 -0800

    task 0x01

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            i = chr(ord(i) - (ord('a') - ord('A')))
+            print("{}".format(i), end='')
+        else:
+            print("{}".format(i), end='')
+    print('')
