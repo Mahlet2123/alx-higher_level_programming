7086a9442cefad18b9a7e8d871d51914a17f0c89
mahlet <akotet2123@gmail.com>
2022-12-03 04:03:45 -0800

    task 0x03

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
             return my_list
         elif idx > len(my_list):
             return my_list
-        else:
-            my_list[idx] = element
-            return my_list
+        #else:
+        my_list[idx] = element
+        return my_list
