2dab22fa61b0bd24c83916ac4c6a116299d8a500
mahlet <akotet2123@gmail.com>
2022-12-04 02:03:11 -0800

    task 0x03

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
+    for i in range(len(matrix)):
+        print()
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=" ")
+    print()
