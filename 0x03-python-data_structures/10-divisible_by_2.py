a65e3d81f099cdd74a355ea211d35aa7e68a7277
mahlet <akotet2123@gmail.com>
2022-12-05 18:03:20 -0800

    task 0x03

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return my_list
     new_list = []
     for i in my_list:
         if i % 2:
