754719e8ff30b22ff180d9e309bb7d5755702c15
mahlet <akotet2123@gmail.com>
2022-12-05 17:45:33 -0800

    task 0x03

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_num = my_list[0]    
+    max = my_list[0]
     for i in my_list:
-        if i > max_num:
-            return i
-
+        if i > max:
+            max = i
+    return max
