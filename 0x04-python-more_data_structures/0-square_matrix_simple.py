cfbcaabac27b6bfcf872812cfb37b434ca52efee
mahlet <akotet2123@gmail.com>
2022-12-07 05:39:00 -0800

    task 0x03

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-    new_matrix = [[ item**2 for item in row] for row in matrix]
+    new_matrix = [[item**2 for item in row]for row in matrix]
     return new_matrix
