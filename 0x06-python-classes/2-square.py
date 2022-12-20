18358e4ff94906d9e0b430ad9ce2a2e0325755f7
mahlet <akotet2123@gmail.com>
2022-12-20 05:47:49 -0800

    task 0x06

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a class Square that defines a square by: (based on 1-square.py)
+
+        Private instance attribute: size
+        Instantiation with optional size: def __init__(self, size=0):
+        size must be an integer, otherwise raise a TypeError exception with
+        the message size must be an integer
+
+        if size is less than 0, raise a ValueError exception with
+        the message size must be >= 0
+
+        You are not allowed to import any module """
+
+
+class Square:
+    """ define a class called square """
+    def __init__(self, size=0):
+        """ Instantiation with optional size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
