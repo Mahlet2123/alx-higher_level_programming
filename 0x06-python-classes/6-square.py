951315985506a8bcd77b3f41d6a67d6ca7cb2a9a
mahlet <akotet2123@gmail.com>
2022-12-22 10:51:20 -0800

    task 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,18 +11,18 @@
     Private instance attribute: position:
     property def position(self): to retrieve it
     property setter def position(self, value): to set it:
-        - position must be a tuple of 2 positive integers, otherwise 
-            raise a TypeError exception with the 
+        - position must be a tuple of 2 positive integers, otherwise
+            raise a TypeError exception with the
                 - message position must be a tuple of 2 positive integers
 
-    Instantiation with optional size and optional position: 
+    Instantiation with optional size and optional position:
         def __init__(self, size=0, position=(0, 0)):
     Public instance method: def area(self):
         that returns the current square area
     Public instance method: def my_print(self):
         that prints in stdout the square with the character #:
             - if size is equal to 0, print an empty line
-            - position should be use by using space - 
+            - position should be use by using space -
                 Don’t fill lines by spaces when position[1] > 0
 
     You are not allowed to import any module """
