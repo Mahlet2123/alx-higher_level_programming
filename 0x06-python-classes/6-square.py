3e5d4d8c8d7e97464d478e03b0b0398a099ea827
mahlet <akotet2123@gmail.com>
2022-12-22 11:05:04 -0800

    task 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,7 +69,10 @@ class Square:
 
     def my_print(self):
         """Public instance method"""
-        [print() for new_line in range(0, self.__position[1])]
+        if self.__size == 0:
+            print()
+        else:
+            [print() for new_line in range(1, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for space in range(self.__position[0])]
             [print("#", end="") for j in range(self.__size)]
