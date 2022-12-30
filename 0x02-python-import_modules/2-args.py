ff40d865828b52fda5d7535354256c1f3979d8d7
mahlet <akotet2123@gmail.com>
2022-12-30 00:56:35 -0800

    task 0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     index = 1
     if argv_count == 0:
         print('{} arguments.'.format(argv_count))
-    if argv_count >= 1:
+    if argv_count == 1:
+        print('{} arguments:'.format(argv_count))
+        print('{:d}: {:s}'.format(index, sys.argv[1]))
+    if argv_count > 1:
         print('{} arguments:'.format(argv_count))
         while (index <= argv_count):
             print('{:d}: {:s}'.format(index, sys.argv[index]))
