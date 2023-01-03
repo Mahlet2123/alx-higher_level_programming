6e5d065a086bebf03534395abf6d32722d9b6db0
mahlet <akotet2123@gmail.com>
2023-01-02 18:57:21 -0800

    task 0x08

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -65,6 +65,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest of the two rectangles"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
