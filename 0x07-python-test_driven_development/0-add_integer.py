aa266593e1eb2d07be684ffb1ca981e27546f66a
mahlet <akotet2123@gmail.com>
2023-01-03 22:12:42 -0800

    0x07

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
-
-
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    Args:
-        a: the first integer.
-        b: the second integer, default 98.
-
-    Raises:
-        TypeError: if a, b are not int, float.
-
-    Returns:
-        The sum of the two integers.
+    """a function that adds 2 integers.
+       Returns an integer: the addition of a and b
     """
-
     if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
+        raise TypeError("a must be an integer")
     if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
 
 if __name__ == "__main__":
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,1 +1,1 @@
-## 0x07. Python - Test-driven development
+# 0x07-python-test_driven_development
