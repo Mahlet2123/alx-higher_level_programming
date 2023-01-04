47347c663a0701785bd8649d6624bff88685e5f7
mahlet <akotet2123@gmail.com>
2023-01-04 05:50:42 -0800

    task 0x07

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is <first name> <last name>
+
+    first_name and last_name must be strings
+    """
+    if  not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
