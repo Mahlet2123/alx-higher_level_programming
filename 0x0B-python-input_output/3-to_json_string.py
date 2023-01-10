d3f667a6328c823aa3de7b7d96f68143fa3e011a
mahlet <akotet2123@gmail.com>
2023-01-10 05:46:36 -0800

    task 0x0B

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" JSON representation of an object (string) """
+
+import json
+
+def to_json_string(my_obj):
+    """ A function that returns the JSON representation
+        of an object (string): """
+    return (json.dumps (my_obj))
