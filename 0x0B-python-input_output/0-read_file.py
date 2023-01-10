6aba75dfe99bb69dbf1efeb2adae34e486e7a00c
mahlet <akotet2123@gmail.com>
2023-01-10 04:34:45 -0800

    task 0x0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" a method to read a file """
+
+
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, 'r', encoding="utf-8") as f:
