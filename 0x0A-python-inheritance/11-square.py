3fbc85abf6c25daf4c38c2666b48dbda14edeb58
mahlet <akotet2123@gmail.com>
2023-01-11 14:44:48 -0800

    task 0x0A

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Geometry module """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle """
+    def __init__(self, size):
+        """ Instantiation with size """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
