33015e3cd427b6737a3e8ee6c4256be07e43ea6e
mahlet <akotet2123@gmail.com>
2023-01-11 01:00:21 -0800

    task 0x0A

--- 0x0A-python-inheritance/befor/0-lookup.py
+++ 0x0A-python-inheritance/befor/0-lookup.py
@@ -4,5 +4,5 @@
 
 def lookup(obj):
     """ returns the list of available
-        attributes and methods of an object"""
-    return (dir(obj))
+    attributes and methods of an object"""
+    return dir(obj)
