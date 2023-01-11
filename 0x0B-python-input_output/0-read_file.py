017e46105d160340992748225e0b4e4e7aaa37f9
mahlet <akotet2123@gmail.com>
2023-01-11 13:50:36 -0800

    task 0x0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, 'r', encoding="utf-8") as f:
         f = f.read()
-        print (f, end="")
+        print(f, end="")
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ object (Python data structure) represented by a JSON string """
-
-
 import json
 """ imports the module """
+
+
 def from_json_string(my_str):
     """ A function that returns the an object (Python data structure)
         represented by a JSON string"""
