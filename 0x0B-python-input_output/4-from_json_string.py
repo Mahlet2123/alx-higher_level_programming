addfdec1b4e3870d5a606a337018f6b097fcf268
mahlet <akotet2123@gmail.com>
2023-01-11 05:25:58 -0800

    task 0x0B

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,4 +7,4 @@ import json
 def from_json_string(my_str):
     """ A function that returns the an object (Python data structure)
         represented by a JSON string"""
-    return (json.loads (my_str))
+    return (json.loads(my_str))
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """ A function that writes an Object to a text file,
         using a JSON representation """
-    my_obj = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(my_obj)
+        f.write(json.dumps(my_obj))
