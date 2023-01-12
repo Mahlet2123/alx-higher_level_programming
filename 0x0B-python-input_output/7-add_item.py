3443f8e35c15e6a2c5614d65ca1be001c79ed82c
mahlet <akotet2123@gmail.com>
2023-01-12 05:51:07 -0800

    readme

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,9 @@
 """Module for a script that adds all arguments to a Python list"""
 import json
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
 file_name = "add_item.json"
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-alx-higher_level_programming
+# alx-higher_level_programming
+
+# Description
+
+## ALX's curriculum for high-level programming. These projects are meant to help us learn how to use and apply the basic fundamental concepts of higher-level programming on a different programming language.
