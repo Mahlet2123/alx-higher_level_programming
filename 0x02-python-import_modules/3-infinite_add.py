cdd53a2fdf92c3286e20f21bfcf3d4e1c434f18c
mahlet <akotet2123@gmail.com>
2023-01-12 12:24:08 -0800

    task 0x02

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,11 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     argv_count = len(argv)
+    all_sum = 0
+    i = 2
     if argv_count == 0:
         print("0")
-    else:
-        while 
-        print("{}".format(x))
+    for i in range(argv_count):
+        all_sum += int(argv[i])
+        i += 1
+    print("{}".format(all_sum))
