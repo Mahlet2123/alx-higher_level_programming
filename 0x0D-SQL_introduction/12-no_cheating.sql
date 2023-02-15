a1507b589e7815794c1f95b42943ccbf4863e6dc
mahlet <akotet2123@gmail.com>
2023-02-15 06:06:31 -0800

    correction

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,2 @@
--- updates the score of Bob to 10
--- Not allowed to use id val, use name field
-UPDATE second_table
-SET score = 10
-WHERE name = "Bob";
+-- script that updates the score of Bob to 10 in the table second_table
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
