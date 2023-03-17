33ce0f08a2f2d1d152094149d7d231c708da403d
mahlet <akotet2123@gmail.com>
2023-03-17 01:31:31 -0700

    created user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
 -- a script that creates the MySQL server user user_0d_1
-<<<<<<< HEAD
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-=======
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
->>>>>>> d2676a4e5f15daa213c0c663434788191802cb10
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
+GRANT  ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
