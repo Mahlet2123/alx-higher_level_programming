4106235d06193baa59d60b31789fda1a1676984a
mahlet <akotet2123@gmail.com>
2023-03-17 01:50:33 -0700

    unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- script that creates the table unique_id
-CREATE TABLE If NOT EXISTS unique_id(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE If NOT EXISTS unique_id(id INT DEFAULT '1' UNIQUE, name VARCHAR(256));
