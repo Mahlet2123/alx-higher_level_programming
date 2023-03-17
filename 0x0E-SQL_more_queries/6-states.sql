b71b076b138182c46cba9ec35d4a9379aee0d40c
mahlet <akotet2123@gmail.com>
2023-03-17 02:05:30 -0700

    create DB and table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,3 @@
--- script that creates the database hbtn_0d_usa and the table states
+-- a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
