b47630e0e5a0b49784738657966e30e0fa703d8e
mahlet <akotet2123@gmail.com>
2023-03-17 02:16:52 -0700

    foriegn key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,3 @@
--- creates db hbtn_0d_usa
-
+-- a script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- create table cities
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+CREATE TABLE IF NOT EXISTS  hbtn_0d_usa.cities(id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
