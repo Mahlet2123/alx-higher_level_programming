90001b078ec312e494b23c86703c7f66b0ec64f1
mahlet <akotet2123@gmail.com>
2023-03-18 00:46:14 -0700

    filter name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM hbtn_0e_0_usa.states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
