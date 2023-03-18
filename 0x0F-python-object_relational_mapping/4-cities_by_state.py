fb2b7fd7794989f78cc6c13568a8df4b2ac9d402
mahlet <akotet2123@gmail.com>
2023-03-18 07:47:00 -0700

    list data in cities table

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,13 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+        """
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
