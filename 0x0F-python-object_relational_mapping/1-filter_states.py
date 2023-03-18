6fc12caec1cac1d9f718c649507b979ffd8e90d5
mahlet <akotet2123@gmail.com>
2023-03-18 06:38:57 -0700

    PEP fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == "N":
