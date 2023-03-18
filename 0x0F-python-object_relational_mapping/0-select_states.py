0b839856c5c1cbf31a0304ce3aad8f2c97098599
mahlet <akotet2123@gmail.com>
2023-03-18 01:16:53 -0700

    debugging first task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,14 @@
 List states Module from the database hbtn_0e_0_usa
 """
 
-
-import MySQLdb
-from sys import argv
-
-
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3]
     )
+
     cur = db.cursor()
     cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id ASC")
     rows = cur.fetchall()
