679831293f79f7c9e07545b0f9809a2e507c87a0
mahlet <akotet2123@gmail.com>
2023-03-18 05:58:09 -0700

    PEP

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""List states Module from the database hbtn_0e_0_usa"""
+"""
+List states Module from the database hbtn_0e_0_usa
+with a name starting with N (upper N)
+"""
 
 
 if __name__ == "__main__":
 	import MySQLdb
 	from sys import argv
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM hbtn_0e_0_usa.states WHERE name LIKE 'N%' ORDER BY \
-states.id ASC"
+	db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+	cur = db.cursor()
+	cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
     )
-    rows = cur.fetchall()
-    for row in rows:
-	    if (row[1][0] == 'N'):
-            print(row)
-    cur.close()
-    db.close()
+	rows = cur.fetchall()
+	for row in rows:
+		if (row[1][0] == 'N'):
+			print(row)
+	cur.close()
+	db.close()
