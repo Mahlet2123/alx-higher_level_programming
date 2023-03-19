0e541932ac9bf01815f7aa950628fab3322a41a4
mahlet <akotet2123@gmail.com>
2023-03-19 02:27:12 -0700

    filter first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,5 +13,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(instance.id, instance.name))
+    if instance is not None:
+        print("{}: {}".format(instance.id, instance.name))
+    else:
+        print()
     session.close()
