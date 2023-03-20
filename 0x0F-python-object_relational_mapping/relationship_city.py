1b279a82da9effb86c3c872169694340e38acc91
mahlet <akotet2123@gmail.com>
2023-03-19 17:45:57 -0700

    relationships

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" City class module"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+Base = declarative_base()
+
+
+class City (Base):
+    """City class"""
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
