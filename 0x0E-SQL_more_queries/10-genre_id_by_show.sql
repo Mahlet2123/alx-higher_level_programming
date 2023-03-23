1636e9b337a804fad92c41350b18ea15bd9d3268
mahlet <akotet2123@gmail.com>
2023-03-23 02:04:00 -0700

    inner join  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,2 @@
--- lists all shows contained in hbtn_0d_tvshows
-
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-INNER JOIN tv_show_genres
-WHERE tv_shows.id  = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id;
