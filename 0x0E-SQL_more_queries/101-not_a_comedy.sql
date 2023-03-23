80fe9adbc9eeba18396108bcd4dd2992a61d3aa9
mahlet <akotet2123@gmail.com>
2023-03-23 11:39:45 -0700

    lists all shows without the genre Comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,2 @@
--- lists all non-Comedy shows
-
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.title NOT IN (
-	SELECT tv_shows.title
-	FROM tv_shows
-	INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-	WHERE tv_genres.name = "Comedy")
-ORDER BY tv_shows.title;
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy") ORDER BY tv_shows.title;
