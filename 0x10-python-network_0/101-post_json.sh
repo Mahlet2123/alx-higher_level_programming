a90ccecf184d1ea92a4a61ead610a9ad10d81ab0
mahlet <akotet2123@gmail.com>
2023-03-31 05:52:25 -0700

    POST json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# posts json file
-curl -s -X POST -d @"$2" -H "Content-Type: application/json" "$1"
+# This script displays all HTTP methods the server will accept.
+curl -sX POST -d @$2 -H "Content-Type: application/json" $1
