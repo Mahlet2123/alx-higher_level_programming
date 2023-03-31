0e2ce2fc537a2db0c5575e7ec8dd79422cf23b9a
mahlet <akotet2123@gmail.com>
2023-03-30 21:56:30 -0700

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-# sends a req to a URL and displays size of response
-curl -so /dev/null -w '%{size_download}\n' $1
+
+# Check if a URL was provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <url>"
+    exit 1
+fi
+
+# Send a request using curl and store the response body in a variable
+response=$(curl -s -w '\n%{size_download}' $1)
+
+# Extract the size of the response body from the last line of the response
+size=$(echo "$response" | tail -n 1)
+
+# Display the size of the response body in bytes
+echo "$size"
