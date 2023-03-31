f8424045ef801330f72e1e58af582a824b8bf17e
mahlet <akotet2123@gmail.com>
2023-03-31 03:33:12 -0700

    Display only body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a req to a URL and displays size of response
-curl -s -L -w "\n%{http_code}\n" $1 | sed -n '/^200$/{:a;n;p;ba;}'
+# Display only body of a 200 status code response
+curl -s -L -w "\n%{http_code}\n" $1
