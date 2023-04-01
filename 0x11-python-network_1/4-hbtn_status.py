13e9f4b521c35f1cc4795b2a458dcdc7c97e4d0f
mahlet <akotet2123@gmail.com>
2023-03-31 17:52:00 -0700

    fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+    string = "Body response:\n\t- type: {}\n\t- content: {}"
+    print(string.format(type(body.decode()), body))
