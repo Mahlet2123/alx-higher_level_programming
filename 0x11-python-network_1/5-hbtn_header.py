901bbc94c39acef150b81527cf34d6291ff6bea6
mahlet <akotet2123@gmail.com>
2023-03-31 18:56:50 -0700

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""  displays the value of the X-Request-Id variable in the header """
+""" displays the value of the X-Request-Id variable in the header """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = requests.get(url)
+    req = requests.get(sys.argv[1])
     print(req.headers['X-Request-Id'])
