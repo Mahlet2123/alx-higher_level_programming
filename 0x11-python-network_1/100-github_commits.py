003deee48aa07c769b82321ee3951db1dc28baf7
mahlet <akotet2123@gmail.com>
2023-04-02 21:27:42 -0700

    list 10 commits (from the most recent to oldest) of the repository “rails”

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,18 @@ import sys
 if __name__ == "__main__":
     repo = sys.argv[1]  # Repository name
     owner = sys.argv[2]  # Owner name
-    url = 'https://api.github.com/repos/{}/{}/commits?author=rails&per_page=10'.format(owner, repo)
+    url = "https://api.github.com/repos/{}/{}/commits?\
+            author=rails&per_page=10".format(
+        owner, repo
+    )
 
     response = requests.get(url)
     commits = response.json()
 
     for commit in commits:
-       print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
+        print(
+            "{}: {}".format(
+                commit.get("sha"),
+                commit.get("commit").get("author").get("name")
+            )
+        )
