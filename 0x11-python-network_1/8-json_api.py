74347660f623f4c47744391a31426da9a7c09901
mahlet <akotet2123@gmail.com>
2023-04-02 21:56:34 -0700

    bug fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
             print('[{}] {}'.format(res_json.get('id'), res_json.get('name')))
         else:
             print('No result')
-    except TypeError:
+    except ValueError:
         print('Not a valid JSON')
