4ea36f1740db96f69663940af7c5e0037558e03e
mahlet <akotet2123@gmail.com>
2023-04-02 21:36:23 -0700

    bug fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
-"""
+"""takes letter and sends a POST req to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    if (len(sys.argv) == 1):
-        values = {"q": ''}
-    else:
-        values = {"q": sys.argv[1]}
-
-    res = requests.post(url, data=values)
+if __name__ == '__main__':
+    letter = {'q': argv[1][0] if len(argv) > 1 else ''}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=letter)
     try:
-        res_json = res.json()
-        if (res_json):
-            print("[{}] {}".format(res_json.get('id'), res_json.get('name')))
-            """print(type(res_json.get('name')))"""
+        response = r.json()
+        if response:
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
         else:
-            print("No result")
-    except TypeError:
-        print("Not a valid JSON")
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
