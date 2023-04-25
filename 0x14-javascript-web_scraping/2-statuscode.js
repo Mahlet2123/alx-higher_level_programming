26a7c2ae49b625a63ec64a7ca62f56902ab4a579
mahlet <akotet2123@gmail.com>
2023-04-25 11:05:48 -0700

    print status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,5 @@ const request = require('../../../usr/lib/node_modules/request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('code:', response.statusCode);
-  }
+  console.log('code:', response.statusCode);
 });
