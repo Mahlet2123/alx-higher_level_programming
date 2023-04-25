fdda0c0e9ec4fecb52a3bfdf9f15eb3c8184cb0a
mahlet <akotet2123@gmail.com>
2023-04-25 10:57:57 -0700

    print status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require('/usr/lib/node_modules/request');
+const request = require('../../../usr/lib/node_modules/request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
