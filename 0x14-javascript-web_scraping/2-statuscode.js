a7ae44922290cc890b33d0bf043cad5f7be718d9
mahlet <akotet2123@gmail.com>
2023-04-25 11:56:16 -0700

    starwars api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const request = require('/usr/lib/node_modules/request');
+const request = require('NODE_PATH');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error)
-    } else {
-        console.log(`code: ${response.statusCode}`);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
