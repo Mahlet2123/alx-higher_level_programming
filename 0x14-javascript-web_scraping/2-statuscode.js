beadde4b145bf3a0faba173f64d8da8999c170b2
mahlet <akotet2123@gmail.com>
2023-04-25 11:24:06 -0700

    bug fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const request = require('../../../usr/lib/node_modules/request');
+const request = require('/usr/lib/node_modules/request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
     if (error) {
         console.error(error)
     } else {
-        console.log('code:', response.statusCode);
+        console.log(`code: ${response.statusCode}`);
     }
 });
