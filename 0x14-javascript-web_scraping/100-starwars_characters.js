b7e7e1ade873382b48c688ee56a68135df9ecd9e
mahlet <akotet2123@gmail.com>
2023-04-26 11:45:51 -0700

    names of characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,29 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films';
-const movieId = process.argv[2];
-
+const request2 = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const results = JSON.parse(body).results;
-    for (const i in results) {
-        if (i == movieId) {
-            const characters = results[i].characters;
-            for (const j in characters) {
-                request.get(characters[j], (error, response, body) => {
-                    if (error) {
-                        console.error(error);
-                    } else {
-                        const name = JSON.parse(body).name
-                        console.log(name);
-                    }
-                });
-            }
+    const characters = JSON.parse(body).characters;
+    for (const j in characters) {
+      request2.get(characters[j], (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
-      }
+      });
     }
+  }
 });
