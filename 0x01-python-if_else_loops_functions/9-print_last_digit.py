6558f7fb7e924d67fae96aa1388c0b18c823833a
Mahlet Seifu <110999391+Mahlet2123@users.noreply.github.com>
2023-12-15 16:46:00 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-# print_last_digit: function that print last digit of any number
-# Number: arguments that hold pass variable or value
-# last: hold last digit of passed argument
-# Return: Last digit
-
+"""
+function that prints the last digit of a number.
+"""
 def print_last_digit(number):
-    last_num =  abs((number % 10))
+    if number < 0:
+        last_num =  ((-1 * number) % 10)
+    else:
+        last_num = number % 10
     print("{}".format(last_num), end="")
     return last_num
